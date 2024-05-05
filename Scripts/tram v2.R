991c3c259232deebd0d2a804c8373ab051be2d9e
kirstinlyon <kirstin@mltwelve.com>
2024-05-05 14:32:52 +0200

    Completed code to pull the list of relevant competitions from a list.

--- Scripts/tram v2.R
+++ Scripts/tram v2.R
@@ -1,35 +1,46 @@
 library(tidyverse)
 library(httr)
 library(jsonlite)
+library(lubridate)
+library(here)
+
+#TODO add error messages for stability + document
+#TODO how to get the country and host? does it even matter? 
+#TODO fix so relative path
+#TODO create link to csvs for all competitions, but split by year. No need to download everything each time.
+#TODO download and bind per year and save in a folder
+#TODO turn "pulling data" in to a function 
 
 
 api_url <- "http://sporttech.io/api/events/"
 
 my_raw_result <- httr::GET(api_url)
 
-str(my_raw_result)
-str(my_raw_result$content)
-
-
 my_content <- httr::content(my_raw_result, as = "text")
-str(my_content)
 
 my_content_from_json <- jsonlite::fromJSON(my_content)
-glimpse(my_content_from_json)
 
-event_df <- my_content_from_json$event_id
+all_events <- my_content_from_json$event
 
-test <- my_content_from_json[["event"]][["00276cc1-8288-429f-45ae-5d8bb2582c28"]]$event_id
+event_data_unflatten <- all_events %>%
+    map(~.[c("event_id","en_name", "sport", "rules", "begin_date", "end_date")]) %>%
+    bind_rows() %>% 
+    mutate(begin_date = ymd_hms(begin_date),
+           end_date = ymd_hms(end_date)) %>% 
+    filter(!is.na(begin_date) & !is.na(end_date)) %>% 
+    filter(begin_date < Sys.Date())
 
-test <- my_content_from_json$event
-
-glimpse(test)
 
 
-#next:  I want to keep only events that have happened - and have a start_date and end_date within a range
+# Specify the full path to the output file
+output_file <- "C:/Users/kirst/Sync/Projects/trampoline/Dataout/list_of_competitions.csv"
 
-event_ids <- lapply(test, function(event) event[["event_id"]])
-event_ids_k <- unlist(event_ids) %>% 
-    as_tibble()
+# Write to CSV file
+write_csv(event_data_unflatten, output_file)
 
-print(event_ids_k)
+   
+
+
+
+
+
