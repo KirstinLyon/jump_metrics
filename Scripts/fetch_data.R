47b7b2042d347ba576196e9aa72b1a453e999a92
kirstinlyon <kirstin@mltwelve.com>
2025-04-20 15:04:09 +0200

    simplified - changed to include specific competitions instead of remove other competitions

--- Scripts/fetch_data.R
+++ Scripts/fetch_data.R
@@ -8,7 +8,7 @@
 library(kickout)
 library(lubridate)
 library(dplyr)
-library(cld2)
+
 library(stringr)
 library(readr)
 
@@ -17,43 +17,12 @@ library(readr)
 
 START_YEAR <- 2023
 
-
-REMOVE_LANGUAGE <- c("ru", "ja", "bg", "af",
-                     "de","es", "et", "fi", "fr",
-                     "pt", "sr", "sv", "lv",
-                     "hmn", "it", "mg")
-
 # Define each pattern on a new line for readability
-EVENTS <- c(
-    "QLD", "VIC", "ACT", "TAS", "STATE",
-    "Midlands", "Power", "Campeonato",
-    "Rocket", "Tumbling", "Territorial",
-    "Island", "Qualificativa", "Barmstedt",
-    "Abertura", "Portimão", "State", "Grenchner",
-    "AGC", "TRP", "Wölfe", "Olympia", "Dynamic",
-    "hoppet", "UVGK", "TCI", "Test", "Interclub",
-    "Hoppet", "AUS", "CSG", "test", "Asian", "Balder",
-    "BGS", "Demo", "KGK", "South", "USM", "TORNEIO",
-    "Kids", "Reguengos", "NSW", "KTF", "HCA",
-    "Soorser", "NZ", "Schools", "Dutch", "FinCup", 
-    "China", "Pokal", "Loulé", "Tri Star", "bounce",
-    "Peke", "WA", "AGA", "AGS", "SM", "Happy", "Basilea",
-    "Bounce", "Levo", "Uppsala", "Nissen", "Oeiras", "Beyond",
-    "Sacros", "COTTBUS", "DTO", "Herbstcup","Chablais",
-    "CMGI","Vic","Coimbra", "Dobrovolsky", "Espoon", 
-    "Latvian", "Merino", "TV", "Friendship", "HTT", "Område",
-    "Geneva", "Herbst", "Apuramento", "BounCe", "High", "Peter",
-    "Scalabiscup", "Summit", "Väst", "MTGA", "AL FUNraiser",
-    "Teamcup", "EsTT", "Trial", "Traps", "Eesti", "závod",
-    "Gefle", "CZE", "České", "Peak", "RUSSIA", "Juegos", "træf",
-    "lēcien"
-)
-
-
-
+EVENTS <- c("DanCup", "Scandinavian Open", "Forbunds", "DM Senior",
+            "Nordic", "Frivolten", "FIG", "DM u12")
 
 # Combine patterns into a single string with | as the separator
-REMOVE_EVENTS <- paste(EVENTS, collapse = "|")
+KEEP_EVENTS <- paste(EVENTS, collapse = "|")
 
 
 # FETCH DATA --------------------------------------------------------
@@ -61,16 +30,8 @@ REMOVE_EVENTS <- paste(EVENTS, collapse = "|")
 # Fetch list of competitions
 event_list <- kickout::fetch_past_event_list() |> 
     kickout::process_event_list() |> 
-    mutate(language = cld2::detect_language(en_name)) |> 
-    filter(year(begin_date) >= START_YEAR,
-           !(language %in% REMOVE_LANGUAGE)
-    ) |> 
-    
-    # Remove unnecessary data - related to language and events 
-    mutate(en_name_trunc = str_sub(en_name, start = 2, end =  10)) |> 
-    filter(str_detect(en_name_trunc, "^[a-zA-ZÀ-ÿ0-9\\p{P}\\sº]+$")) |> 
-    filter(!str_detect(en_name, REMOVE_EVENTS)) |> 
-    select(-en_name_trunc, -language) 
+    filter(year(begin_date) >= START_YEAR) |> 
+    filter(str_detect(en_name, KEEP_EVENTS)) 
 
 
 # CREATE DATASETS ------------------------------------------------------------------
@@ -143,5 +104,4 @@ club <- events |>
 DK_name_club <- events |> 
     select(name, club, country) |> 
     distinct()
-
 
