6989307f17ec72a786702e8e45f22166944adbb0
kirstinlyon <kirstin@mltwelve.com>
2025-04-25 09:30:16 +0200

    cleaned up files and moved data preparation to fetch_data.  Final dataset to be uploaded to github

--- Scripts/fetch_data.R
+++ Scripts/fetch_data.R
@@ -10,9 +10,10 @@ library(dplyr)
 library(stringr)
 
 
+
 # GLOBAL VARIABLES -------------------------------------------------
 
-START_YEAR <- 2025
+START_YEAR <- 2023
 
 # Define each pattern on a new line for readability
 EVENTS <- c("DanCup", "Scandinavian Open", "Forbunds", "DM Senior",
@@ -24,14 +25,16 @@ KEEP_EVENTS <- paste(EVENTS, collapse = "|")
 
 # FETCH DATA --------------------------------------------------------
 
+
 # Fetch list of competitions
 event_list <- kickout::fetch_past_event_list() |> 
     kickout::process_event_list() |> 
     
     #Use base R to filter dates - avoid calling the lubridate library
     filter(as.integer(format(begin_date, "%Y")) >= START_YEAR,
-    str_detect(en_name, KEEP_EVENTS)
+           str_detect(en_name, KEEP_EVENTS)
     ) 
+
 
 
 # CREATE DATASETS ------------------------------------------------------------------
@@ -48,16 +51,21 @@ events <- purrr::map(event_list$event_id, ~ kickout::fetch_event_url(.x, event_l
                   Execution = execution,
                   Mark = mark,
                   Total = mark_total,
-                  'Routine Number' = routine_number,
                   Stage = stage,
                   Competition = competition,
                   Discipline = discipline,
                   '# Elements' = number_elements,
                   Rank = rank
     ) |> 
+    mutate(Date = as.Date(Date),
+           Stage = case_when(str_detect(Stage,"inal") ~ Stage,
+                             TRUE ~ paste(Stage, routine_number, sep ="_")
+                             )
+           )|> 
     select(event_uuid, Date, Event,   Discipline, Competition, Competitor, Club, Country, Stage, 
-           'Routine Number', Rank, Total, Mark, '# Elements', Execution, T, H, D, everything()) 
-
+           Rank, Total, Mark, '# Elements', Execution, T, H, D, everything()) |> 
+    arrange(desc(Date), Discipline, Competition, group_number, performance_number, Competitor) |> 
+    select(-c(group_number, performance_number))
 
 
 
