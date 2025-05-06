31566bf55b58fb0d4d4c6d0dbeb0e360fe49b397
kirstinlyon <kirstin@mltwelve.com>
2025-05-06 19:59:57 +0200

    further debugging

--- Scripts/test.R
+++ Scripts/test.R
@@ -1,14 +1,32 @@
 
+library(dplyr)
+library(readr)
+
+events <-  read_csv("https://raw.githubusercontent.com/KirstinLyon/jump_metrics/refs/heads/main/Dataout/all_events.csv")
 
 # Check for DK clubs - look at UNKNOWN
 club <- events |> 
-    select(club, country) |> 
+    select(Club, Country) |> 
+    distinct() |> 
+    arrange(Club)  
+
+den_club <- club |> 
+    filter(Country == "DEN") |> 
+    select(Club) 
+
+unknown_country <- club |> 
+    filter(Country == "unknown") 
+
+unknown_club <- club |> 
+    filter(Club == "unknown") |> 
+    arrange(Country) |> 
     distinct()
 
 # Check for DK clubs - look for unknown countries and names
 DK_name_club <- events |> 
-    select(name, club, country) |> 
+    select(Competitor, Club, Country) |> 
     distinct() |> 
-    filter(country == "DEN", club != "unknown")
+    filter(Country == "DEN", Club != "unknown") |> 
+    arrange(Competitor)
 
 #club <- kickout::data_representing_map
