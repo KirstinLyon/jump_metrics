bf6c1e6be70c657b9a4d6f8125151d09930c562b
kirstinlyon <kirstin@mltwelve.com>
2025-05-24 12:14:39 +0200

    fixing automation

--- Scripts/star_wars_test.R
+++ Scripts/star_wars_test.R
@@ -9,4 +9,5 @@ write_csv(data, "Dataout/starwars.csv")
 data_from_github <- read_csv("https://raw.githubusercontent.com/KirstinLyon/jump_metrics/refs/heads/main/Dataout/starwars.csv")
 
 
-event <- read_csv("Dataout/events.csv")
+event <- read_csv("Dataout/all_events.csv")
+event_from_github <- read_csv("https://raw.githubusercontent.com/KirstinLyon/jump_metrics/refs/heads/main/Dataout/all_events_test.csv")
